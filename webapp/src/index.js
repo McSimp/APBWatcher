06aedb1362687431f39793272e436d81baf0c2a6
Will Donohoe <will@mcsi.mp>
2016-10-27 16:21:49 +1100

    Make web app work in IE11

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
 import configureStore from './store/configureStore';
+import 'core-js/fn/string/starts-with';
+import 'core-js/es6/object';
+import 'core-js/es6/symbol';
+import 'core-js/es6/map';
 
 const store = configureStore({
   instances: [],
